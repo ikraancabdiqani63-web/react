35c33432582eccbf5b50ec0f8e2f99ee8666ecc0
ikraan abdiqani husein <ikraancabdiqani63@gmail.com>
2026-06-15 12:25:30 +0300

    ch1

---
+++ ch1_intro/ikran/src/compounents/Greating.jsx
@@ -0,0 +1,8 @@
+
+
+function Greating(props) {
+  return ( <h1>Hello, {props.name}!</h1>
+     )
+}
+
+export default Greating
---
+++ ch1_intro/ikran/src/compounents/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <div>
+        <h1>This is my first React component</h1>
+    </div>
+
+  )
+}
+
+
+export default Header
